04787043c39aa8b9a8620a4759a79e43a28b369e
xuanxu <xuanxusb@outlook.com>
2021-06-18 00:17:04 +0800

    修改图片上传接口到consumer

--- src/main/java/com/rjgc/controller/ImageFakeController.java
+++ src/main/java/com/rjgc/controller/ImageFakeController.java
@@ -1,18 +1,18 @@
 package com.rjgc.controller;
 
 import com.rjgc.exceptions.ResBody;
-import com.rjgc.feignClient.ImageFakeClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ImageFakeController {
-    @Autowired
-    private ImageFakeClient imageFakeClient;
 
     @GetMapping("/imgFake")
     ResBody<String> uploadImg() {
-        return imageFakeClient.uploadImg();
+        ResBody<String> rb = new ResBody<>();
+        rb.setCode(20000);
+        rb.setMessage("成功");
+        rb.setResult(null);
+        return rb;
     }
 }
